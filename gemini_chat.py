347aef963a60ab2e0b41e63f600e941c9b0bad2a
Aman Dwivedi <amandwivedi73072@gmail.com>
2026-07-10 14:43:23 +0530

    Initial Commit

--- gemini_chat.py
+++ gemini_chat.py
@@ -13,9 +13,8 @@ client = genai.Client(
 # -----------------------------
 # Session State
 # -----------------------------
-
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state["messages"] = []
 
 
 # -----------------------------
@@ -24,12 +23,14 @@ if "messages" not in st.session_state:
 
 def loan_chatbot(user_data):
 
-    st.write("---")
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
 
+    st.write("---")
     st.subheader("🤖 AI Loan Assistant")
 
     # Show old messages
-    for msg in st.session_state.messages:
+    for msg in st.session_state["messages"]:
 
         with st.chat_message(msg["role"]):
             st.markdown(msg["content"])
@@ -41,12 +42,12 @@ def loan_chatbot(user_data):
 
     if prompt:
 
-        st.session_state.messages.append(
-            {
-                "role":"user",
-                "content":prompt
-            }
-        )
+        st.session_state["messages"].append(
+    {
+        "role": "user",
+        "content": prompt
+    }
+    )
 
         with st.chat_message("user"):
             st.markdown(prompt)
@@ -103,9 +104,9 @@ Answer politely in simple English.
 
                 st.markdown(answer)
 
-                st.session_state.messages.append(
-                    {
-                        "role":"assistant",
-                        "content":answer
-                    }
+                st.session_state["messages"].append(
+                {
+                    "role": "assistant",
+                    "content": answer
+                }
                 )
